772c3747b29e65fe88598eacdd8940fdc52b74ba
satub <satu.barnhill@gmail.com>
2016-08-09 00:51:54 -0400

    Done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def build_network(attributes)
+    network = Network.new(attributes)
+    network.save
+    id = Network.find_by(attributes).id
+    self.network_id = id
+    self.network = network
+    network
+  end
+
 end
