db57929a00cf87bc53255db4638287398401ca1c
Esmery Corniel <esmerycornielle@gmail.com>
2016-08-29 17:33:09 -0400

    Done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    response = ""
+    # "#{self.characters.name} - #{self.characters.shows}"
+    self.characters.each do |character|
+      response = character.name + " - " +character.show.name
+    end
+    response
+  end
+
+
+
 end
